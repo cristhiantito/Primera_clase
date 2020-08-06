835b2cd697b1c7309bd2d794c893b5adffcdf2c3
cristhiantito <cristhianalexandertitocarrasco@gmail.com>
2020-08-06 00:31:40 -0500

    Agregando 2 ejercicios

---
+++ area_rectangulo.py
@@ -0,0 +1,4 @@
+altura = 10
+base = 2
+area = altura * base
+print ("El area del rectangulo es:" + str(area))
---
+++ area_triangulo.py
@@ -0,0 +1,4 @@
+base = 1
+altura = 1
+area = base * altura / 2
+print ("El area del triangulo es:" + str(area))
